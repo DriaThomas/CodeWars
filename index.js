3beb8a4d9a00babbb95976f51d7d0208247b0ca8
Dria Thomas <driathomas96@gmail.com>
2021-11-01 22:21:29 -0400

    update

--- index.js
+++ index.js
@@ -4,4 +4,31 @@
 function multiply(a, b) {
     return a * b
 }
+//
+solve(["abode", "ABc", "xyzD"]) = [4, 3, 1]
+function solve(arr) {
+    let alphabets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    let positionArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let incr = 0;
+        for (let j = 0; j < arr[i].length; j++) {
+
+            for (let n = 0; n < alphabets.length; n++) {
+                if (arr[i][j].toUpperCase() === alphabets[j]) {
+                    incr++;
+                    break;
+                }
+
+            }
+        }
+        positionArr.push(incr);
+    }
+    console.log(arr[0][1].toUpperCase())
+    return positionArr;
+
+
+};
+
+
 
